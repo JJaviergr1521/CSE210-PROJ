980a4c1bb16e7897b6062a0e1d27dfee6a2fe44e
JJaviergr1521 <jjaviergr@byu.edu>
2024-11-10 20:17:55 -0600

    Prep 3 done

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,63 @@
 using System;
+using System.Globalization;
 
 class Program
 {
     static void Main(string[] args)
+
+
     {
-        Console.WriteLine("Hello Prep3 World!");
+
+        Random randomGenerator = new Random();
+        int RandomNumber = randomGenerator.Next(1, 99);
+        string InputNum = "";
+        int NumberGuess = 0;
+        
+        string flag = "true";
+        
+
+        
+        //Console.WriteLine("Guess");
+       
+    
+
+        while (flag == "true")
+
+        {   
+            Console.WriteLine("Guess the magic number");
+            InputNum = Console.ReadLine();
+            NumberGuess = int.Parse(InputNum);
+
+             if ( RandomNumber> NumberGuess)
+        {   Console.WriteLine(RandomNumber);
+            Console.WriteLine("Higher");
+        }
+        else if (RandomNumber < NumberGuess)
+        {   Console.WriteLine(RandomNumber);
+            Console.WriteLine("Lower");
+        }
+        else
+        {
+            Console.WriteLine(RandomNumber);
+            Console.WriteLine("Yep that is the magic number");
+            Console.WriteLine("Do you want to play again? yes/no");
+            string Playagain = Console.ReadLine();
+            if(Playagain == "yes")
+            {
+                flag = "true";
+            }
+            else
+            {
+                flag = "false";
+            }
+            
+        }
+
+            
+            
+
+        } 
+
+    
     }
 }
