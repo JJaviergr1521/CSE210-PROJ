2532f52be2b306dfab121f1b2fd8bdc432fbd251
JJaviergr1521 <jjaviergr@byu.edu>
2024-11-11 09:32:18 -0600

    prep5 activity done

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,56 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+       static void DisplayWelcome()
+
+      {
+
+        Console.WriteLine("Welcome to my program ;)");
+      } 
+
+        static string PromptUserName()
+
+      {
+
+        Console.WriteLine("Please enter your Name:");
+        string name = Console.ReadLine();
+        return name;
+
+
+      }
+
+       static int PromptUserNumber()
+      {
+
+        Console.WriteLine("Please enter your favorite Number:");
+         int userNum = int.Parse(Console.ReadLine());
+         return userNum;
+      }
+
+       static int SquareNumber(int InNumb)
+      {
+
+        InNumb = InNumb * InNumb;
+        return InNumb;
+      }
+
+
+    static void DisplayResult(string User, int squaredN)
+    {
+        Console.Write($"{User}, the square of your number is {squaredN}");
+    }
+
+
+    DisplayWelcome();
+    string UserIn = PromptUserName();
+    int SquNum = PromptUserNumber();
+    DisplayResult(UserIn,SquNum);
+
+
+
+
+
+
+
     }
 }
