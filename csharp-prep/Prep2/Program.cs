2fa077ec48413ca20e61c2243716e67c7f47ba6b
JJaviergr1521 <jjaviergr@byu.edu>
2024-11-10 18:46:03 -0600

    Prep 2 assignment Done

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,47 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("Please write your Grade Percentage");
+        string grade = Console.ReadLine();
+        int Gradenumber = int.Parse(grade);
+        String Letter = "";
+
+        
+
+        if(Gradenumber >= 90)
+        {
+            Letter = "A";
+        }
+        else if (Gradenumber >= 80)
+        {
+            Letter = "B";
+        }
+        else if (Gradenumber >= 70 )
+        {
+            Letter = "C";
+        }
+        else if (Gradenumber >= 60)
+        {
+            Letter = "D";
+        }
+        else
+        {
+            Letter = "F";
+        }
+
+        Console.WriteLine($"your Grade is {Letter}");
+
+        if (Letter == "D" || Letter == "F")
+        {
+            Console.WriteLine("You did not PASS");
+        }
+
+        else
+        {
+            Console.WriteLine("Congrats You did PASS");
+        }
+
+
+
     }
 }
